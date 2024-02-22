87eec402b43ccc621e36b8102ebdac40719e7c6f
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-02-22 15:44:06 +1000

    feat(pkg/agent): configurable execution trace params

--- pkg/agent/ethereum/execution/config.go
+++ pkg/agent/ethereum/execution/config.go
@@ -4,7 +4,10 @@ import "errors"
 
 type Config struct {
 	// The address of the Execution node to connect to
-	NodeAddress string `yaml:"nodeAddress"`
+	NodeAddress         string `yaml:"nodeAddress"`
+	TraceDisableMemory  bool   `yaml:"traceDisableMemory"`
+	TraceDisableStack   bool   `yaml:"traceDisableStack"`
+	TraceDisableStorage bool   `yaml:"traceDisableStorage"`
 }
 
 func (c *Config) Validate() error {
