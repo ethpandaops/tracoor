f2af1d86c03f864b5b1f557acd3196540450353c
Sam Calder-Mason <sam@puritydev.io>
2024-06-05 16:44:06 +1000

    feat(agent): Add feature flags

--- pkg/agent/queue.go
+++ pkg/agent/queue.go
@@ -32,30 +32,50 @@ type ExecutionBadBlockRequest struct {
 }
 
 func (s *agent) enqueueBeaconState(ctx context.Context, slot phase0.Slot) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconState() {
+		return
+	}
+
 	s.beaconStateQueue <- &BeaconStateRequest{
 		Slot: slot,
 	}
 }
 
 func (s *agent) enqueueBeaconBlock(ctx context.Context, slot phase0.Slot) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconBlock() {
+		return
+	}
+
 	s.beaconBlockQueue <- &BeaconBlockRequest{
 		Slot: slot,
 	}
 }
 
 func (s *agent) enqueueBeaconBadBlock(ctx context.Context, path string) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconBadBlock() {
+		return
+	}
+
 	s.beaconBadBlockQueue <- &BeaconBadBlockRequest{
 		Path: path,
 	}
 }
 
 func (s *agent) enqueueBeaconBadBlob(ctx context.Context, path string) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconBadBlob() {
+		return
+	}
+
 	s.beaconBadBlobQueue <- &BeaconBadBlobRequest{
 		Path: path,
 	}
 }
 
 func (s *agent) enqueueExecutionBlockTrace(ctx context.Context, blockHash string, blockNumber uint64) {
+	if !s.Config.Ethereum.Features.GetFetchExecutionBlockTrace() {
+		return
+	}
+
 	s.executionBlockTraceQueue <- &ExecutionBlockTraceRequest{
 		BlockNumber: blockNumber,
 		BlockHash:   blockHash,
@@ -63,10 +83,18 @@ func (s *agent) enqueueExecutionBlockTrace(ctx context.Context, blockHash string
 }
 
 func (s *agent) enqueueExecutionBadBlock(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchExecutionBadBlock() {
+		return
+	}
+
 	s.executionBadBlockQueue <- &ExecutionBadBlockRequest{}
 }
 
 func (s *agent) processBeaconStateQueue(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconState() {
+		return
+	}
+
 	for stateRequest := range s.beaconStateQueue {
 		s.metrics.SetQueueSize(BeaconStateQueue, len(s.beaconStateQueue), s.Config.Name)
 
@@ -90,6 +118,10 @@ func (s *agent) processBeaconStateQueue(ctx context.Context) {
 }
 
 func (s *agent) processBeaconBlockQueue(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconBlock() {
+		return
+	}
+
 	for blockRequest := range s.beaconBlockQueue {
 		s.metrics.SetQueueSize(BeaconBlockQueue, len(s.beaconBlockQueue), s.Config.Name)
 
@@ -113,6 +145,10 @@ func (s *agent) processBeaconBlockQueue(ctx context.Context) {
 }
 
 func (s *agent) processBeaconBadBlockQueue(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconBadBlock() {
+		return
+	}
+
 	for badBlockRequest := range s.beaconBadBlockQueue {
 		s.metrics.SetQueueSize(BeaconBadBlockQueue, len(s.beaconBadBlockQueue), s.Config.Name)
 
@@ -135,6 +171,10 @@ func (s *agent) processBeaconBadBlockQueue(ctx context.Context) {
 }
 
 func (s *agent) processBeaconBadBlobQueue(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchBeaconBadBlob() {
+		return
+	}
+
 	for badBlobRequest := range s.beaconBadBlobQueue {
 		s.metrics.SetQueueSize(BeaconBadBlobQueue, len(s.beaconBadBlobQueue), s.Config.Name)
 
@@ -157,6 +197,10 @@ func (s *agent) processBeaconBadBlobQueue(ctx context.Context) {
 }
 
 func (s *agent) processExecutionBlockTraceQueue(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchExecutionBlockTrace() {
+		return
+	}
+
 	for traceRequest := range s.executionBlockTraceQueue {
 		s.metrics.SetQueueSize(ExecutionBlockTraceQueue, len(s.executionBlockTraceQueue), s.Config.Name)
 
@@ -181,6 +225,10 @@ func (s *agent) processExecutionBlockTraceQueue(ctx context.Context) {
 }
 
 func (s *agent) processExecutionBadBlockQueue(ctx context.Context) {
+	if !s.Config.Ethereum.Features.GetFetchExecutionBadBlock() {
+		return
+	}
+
 	for range s.executionBadBlockQueue {
 		s.metrics.SetQueueSize(ExecutionBadBlockQueue, len(s.executionBadBlockQueue), s.Config.Name)
 
