dd12fd3489dcd6745a22815c4a55f952e26e4d78
Andrew Davis <1709934+Savid@users.noreply.github.com>
2024-08-06 16:41:59 +1000

    cleanup

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -27,10 +27,10 @@ export default defineConfig({
   server: {
     proxy: {
       '/v1': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:7007',
       },
       '/download': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:7007',
       },
     },
   },
